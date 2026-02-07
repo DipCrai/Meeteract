764123c881b47912f848ce88ea915b43db237d98
DipCrai <dipcraitop1gg@gmail.com>
2026-02-07 18:09:13 +0300

    login to HF

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from huggingface_hub import login
+
+from config import HF_TOKEN
 from core.analysis import start_summarize_thread
 from ui.main_window import MainWindow
+
 import app
 
 if __name__ == "__main__":
+    login(HF_TOKEN)
     app.window = window = MainWindow()
     window.set_start_handler(start_summarize_thread)
     window.run()
